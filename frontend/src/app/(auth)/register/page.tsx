c8c38e7292e1b98e9636f5be3042015cc87ba927
scottqueen-bixal <scott.queen@bixal.com>
2025-07-13 07:39:39 -0400

    feat: enhance registration form with action state management and error handling

--- frontend/src/app/(auth)/register/page.tsx
+++ frontend/src/app/(auth)/register/page.tsx
@@ -1,4 +1,8 @@
+"use client"
+
 import Link from 'next/link'
+import { signup } from "@/app/actions/auth"
+import { useActionState } from 'react'
 import { Button } from "@/components/ui/button"
 import {
   Card,
@@ -14,47 +18,86 @@ import { Label } from "@/components/ui/label"
 
 
 export default function Register() {
+  const [state, action, pending] = useActionState(signup, undefined)
+
+  console.log(state)
+
   return (
-  <Card className="w-full max-w-sm">
-    <CardHeader>
-      <CardTitle>Create a new account</CardTitle>
-      <CardDescription>
-        Enter your email below to create a new account
-      </CardDescription>
-    </CardHeader>
-    <CardContent>
-      <form>
-        <div className="flex flex-col gap-6">
+  <form action={action}>
+    <Card className="w-full max-w-sm">
+      <CardHeader>
+        <CardTitle>Create a new account</CardTitle>
+        <CardDescription>
+          Enter your email below to create a new account
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+          <div className="flex flex-col gap-6">
           <div className="grid gap-2">
-            <Label htmlFor="email">Email</Label>
-            <Input
-              id="email"
-              type="email"
-              placeholder="m@example.com"
-              required
-            />
-          </div>
-          <div className="grid gap-2">
-            <div className="flex items-center">
-              <Label htmlFor="password">Password</Label>
+              <Label htmlFor="name">Name</Label>
+              <Input
+                id="name"
+                name="name"
+                type="text"
+                placeholder="Name"
+                defaultValue={state?.formData?.name as string || ''}
+                required
+              />
+              {
+                state?.errors?.name &&
+                  <small className="text-sm leading-none font-medium">{state.errors.name}
+                  </small>
+              }
             </div>
-            <Input id="password" type="password" required />
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                name="email"
+                type="email"
+                placeholder="me@example.com"
+                defaultValue={state?.formData?.email as string || ''}
+                required
+              />
+              {
+                state?.errors?.email &&
+                  <small className="text-sm leading-none font-medium">{state.errors.email}
+                  </small>
+              }
+            </div>
+            <div className="grid gap-2">
+              <div className="flex items-center">
+                <Label htmlFor="password">Password</Label>
+              </div>
+              <Input id="password" name="password" type="password" placeholder='Password' defaultValue={state?.formData?.password as string || ''} required />
+                {state?.errors?.password && (
+                  <div>
+                    <p>
+                      <small className="text-sm leading-none font-medium">Password must:</small>
+                      </p>
+                    <ul>
+                      {state.errors.password.map((error) => (
+                        <li key={error}>- <small className="text-sm leading-none font-medium">{error}
+                        </small>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                )}
+            </div>
           </div>
-          <div className="grid gap-2">
-            <Label htmlFor="confirm-password">Confirm Password</Label>
-            <Input id="confirm-password" type="password" required />
-          </div>
-        </div>
-      </form>
-    </CardContent>
-    <CardFooter className="flex-col gap-2">
-      <Button type="submit" className="w-full">
-        <Link href="/login">Register</Link>
-      </Button>
-      <CardAction>
-        <Button variant="link" ><CardDescription><Link href="/login">Already have an account? Try logging in.</Link></CardDescription></Button>
-      </CardAction>
-    </CardFooter>
-  </Card>
+      </CardContent>
+      <CardFooter className="flex-col gap-2">
+        <Button type="submit" disabled={pending} className="w-full">
+          Register
+        </Button>
+        <CardAction>
+          <CardDescription>
+            <Link href="/login">Already have an account? Try logging in.</Link>
+          </CardDescription>
+        </CardAction>
+      </CardFooter>
+    </Card>
+  </form>
   );
 }
